c0a67f4738e4739e5996570c96bbc270743c1a68
Hernan Vizoso <hernan.vizoso@gmail.com>
2025-10-13 15:33:06 -0300

    clase 4

---
+++ CLASE4/main.py
@@ -0,0 +1,50 @@
+opcion = 1
+
+# match opcion:
+#     case 1 , 2, 3: 
+#         print("ok")
+#     case 4:
+#         print ("bad")
+#     case _:
+#         print("good")
+
+# match 10:
+#     case 10: 
+#         print("10")
+#     case 5:
+#         print ("5")
+#     case _:
+#         print("good")
+        
+# nombre = "gus"
+# temperatura = 2.12345
+
+# texto = "Python"
+
+# if "P" in texto and texto.endswith("on"):
+
+#     print("Condición cumplida.")
+
+# else:
+
+#     print("Condición no cumplida.")
+
+# edad = int(input("Ingresá tu edad: "))
+
+# if edad < 18:
+
+#     print("Sos menor de edad.")
+
+# else:
+
+#     print("Sos mayor de edad.")
+
+# nombre = "Ana"
+
+# saludo = f"Hola, {nombre}!"
+
+# print(saludo)
+
+texto = "Hola"
+
+texto[0] = "J"
